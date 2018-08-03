ffa73e311444b066226cad22b4cb6b3ceb193690
proyectofci <https://github.com/proyectofci/proyecto.git>
2018-08-03 17:58:41 -0500

    Interfaz cultivos

--- Interfaz FCI/js/mostrar-info-cultivo.js
+++ Interfaz FCI/js/mostrar-info-cultivo.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
 	var btnMostrarInfo = $(".cultivo__active");
 	var descripcionCultivo = $("#informacion-cultivo");
+	var main = $("#main");
+	var primeraCarga = true;
 
 	btnMostrarInfo.click(function() {
-		descripcionCultivo.css({'display':'none'});
+		var posicionActual	 = $(document).scrollTop();
+		var posicion = main.offset().top;
+
+		if(primeraCarga){
+			
+			descripcionCultivo.css({'display':'none'});	
+			
+			$("html, body").animate({
+				scrollTop : posicion
+			}, 500, function(){
+				primeraCarga = false;
+			});
+		}
+
+		if(!primeraCarga){
+			descripcionCultivo.children().fadeOut('fast');
+			descripcionCultivo.children().fadeIn('fast');
+		}
+		
 		descripcionCultivo.slideDown('1000');
 	});
 });
